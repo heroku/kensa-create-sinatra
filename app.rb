e775e54a7daea59bc8b38c206370548c51398773
Chris Continanza <christopher.continanza@gmail.com>
2011-11-15 15:44:56 -0800

    make templte more interactive

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ require './env'
 class App < Sinatra::Base
   use Rack::Session::Cookie, secret: ENV['SSO_SALT']
 
+  @@users = []
+
+  class User
+    attr_accessor :plan
+    def initialize(plan)
+      @plan = plan
+    end
+  end
+
   helpers do
     def protected!
       unless authorized?
@@ -19,6 +28,22 @@ class App < Sinatra::Base
       @auth ||=  Rack::Auth::Basic::Request.new(request.env)
       @auth.provided? && @auth.basic? && @auth.credentials && 
       @auth.credentials == [ENV['HEROKU_USERNAME'], ENV['HEROKU_PASSWORD']]
+    end
+
+    def show_request
+      STDOUT.puts "Users: #{@@users.inspect}"
+      body = request.body.read
+      unless body.empty?
+        STDOUT.puts "request body:"
+        STDOUT.puts JSON.parse(body)
+      end
+      unless params.empty?
+        STDOUT.puts "params: #{params.inspect}"
+      end
+    end
+
+    def get_user
+      @@users[params[:id].to_i] or halt 404, 'user not found'
     end
   end
   
@@ -34,6 +59,7 @@ class App < Sinatra::Base
 
   # sso sign in
   get "/heroku/resources/:id" do
+    show_request
     pre_token = params[:id] + ':' + ENV['SSO_SALT'] + ':' + params[:timestamp]
     token = Digest::SHA1.hexdigest(pre_token).to_s
     halt 403 if token != params[:token]
@@ -44,28 +70,34 @@ class App < Sinatra::Base
 
     session[:heroku_sso] = params['nav-data']
     response.set_cookie('heroku-nav-data', value: params['nav-data'])
+
+    @user = get_user
     haml :index
   end
 
   # provision
   post '/heroku/resources' do
+    show_request
     protected!
-    #u = User.create()
-    #{id: u.id, config: {"MYADDON_URL" => 'http://user.yourapp.com'}}.to_json
     status 201
-    {id: 1, config: {"MYADDON_URL" => 'http://user.yourapp.com'}}.to_json
+    @@users << User.new('test')
+    {id: @@users.size+1, config: {"MYADDON_URL" => 'http://user.yourapp.com'}}.to_json
   end
 
   # deprovision
   delete '/heroku/resources/:id' do
+    show_request
     protected!
-    #User.get(params[:id]).destroy
+    @@users.delete(get_user)
     "ok"
   end
 
   # plan change
   put '/heroku/resources/:id' do
+    show_request
     protected!
+    user = get_user 
+    user.plan = params[:plan]
     "ok"
   end
 end
